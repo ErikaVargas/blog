ae9a7ed00a91057763c009f34ec670a2adfadc7c
irmp <hola>
2014-03-28 21:06:01 -0600

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '763d240febee3a89b448f03fd9bd07248aae122f5ef2327d9d6d99880d7ea1820a2cc611c655c3e7b2f128dab98ffd5a6424474d27ad977b3ae1717ebe0b79e7'
